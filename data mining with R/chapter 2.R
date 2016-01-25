6d2d9cc2aea2dde2964e8e742dfe00fe0c8272eb
xiao <zhangshijieno1@163.com>
2016-01-25 22:23:48 +0800

    data preparation for data mining

--- data mining with R/chapter 2.R
+++ data mining with R/chapter 2.R
@@ -19,3 +19,50 @@ z <- equal.count(rnorm(50))
 plot(z)
 print(z)
 print(levels(z))
+lines(density(algae$mxPH,na.rm = T))
+hist(algae$Chla,probability = T)
+lines(density(algae$Chla,na.rm = T))
+algae[is.na(algae$Chla),"Chla"]<-median(algae$Chla,na.rm = T)
+###########通过变量相关性填补数据缺失值
+symnum(cor(algae[,4:18],use = "complete.obs"))
+apply(algae,1,function(x)sum(is.na(x)))
+algae[62,]
+algae[199,]
+data("algae")
+algae<-algae[-manyNAs(algae),]
+lm(PO4~oPO4,data = algae)
+a<-is.na(algae$PO4)
+is.na(algae$PO4)==TRUE
+algae[28,"PO4"]<-42.897+1.293*algae[28,"oPO4"]
+data(algae)
+algae<-algae[-manyNAs(algae),]
+fillPO4<-function(oP){
+        if(is.na(oP))
+                return(NA)
+        else
+                return(42.897+1.293*oP)
+}
+
+algae[is.na(algae$PO4),"PO4"]<-sapply(algae[is.na(algae$PO4),"oPO4"],fillPO4)
+
+histogram(~mxPH|season,data = algae)
+algae$season<-factor(algae$season,c("spring","summer","autumn","winter"))
+lines(density(~mxPH|season,data=algae))
+histogram(~mxPH|size,data = algae)
+histogram(~mxPH|size*speed,data = algae)
+stripplot(size~mxPH|speed,data = algae,jitter=T)
+##########################
+######用观测值间的想相关性填充数据
+
+
+
+
+
+
+
+
+
+
+
+
+
